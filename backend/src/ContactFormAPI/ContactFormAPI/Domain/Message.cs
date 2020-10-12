9d90870a7e0a32892bbcb9afb2aebc809eb099a8
Lourdes Flores <lourdes_fn82@hotmail.com>
2020-10-12 14:15:20 +0200

    add domain class

--- backend/src/ContactFormAPI/ContactFormAPI/Domain/Message.cs
+++ backend/src/ContactFormAPI/ContactFormAPI/Domain/Message.cs
@@ -1,6 +1,14 @@
-﻿namespace ContactFormAPI.Domain
+﻿using System;
+
+namespace ContactFormAPI.Domain
 {
     public class Message
     {
+        public int Id { get; set; }
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
     }
 }
