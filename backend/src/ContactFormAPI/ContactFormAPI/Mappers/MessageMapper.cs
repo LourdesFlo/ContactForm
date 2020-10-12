bcdd92256a0f8747570661bffbecd66b2842276f
Lourdes Flores <lourdes_fn82@hotmail.com>
2020-10-12 18:20:00 +0200

    add Message DTO and domain mapper

---
+++ backend/src/ContactFormAPI/ContactFormAPI/Mappers/MessageMapper.cs
@@ -0,0 +1,37 @@
+﻿using ContactFormAPI.Domain;
+using ContactFormAPI.DTOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactFormAPI.Mappers
+{
+    public class MessageMapper
+    {
+        public Message FromDtoToDomain(MessageDto dto)
+        {
+            return new Message
+            {
+                Id = Guid.NewGuid(),
+                To = dto.To,
+                From = dto.From,
+                Subject = dto.Subject,
+                Body = dto.Body,
+                Date = DateTime.Now
+            };
+        }
+
+        public MessageDto FromDomainToDto(Message message)
+        {
+            return new MessageDto
+            {
+                To = message.To,
+                From = message.From,
+                Subject = message.Subject,
+                Body = message.Body,
+                Date = message.Date
+            };
+        }
+    }
+}
